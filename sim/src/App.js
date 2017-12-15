6e837679a4d9b841013ba1b87bf94cd510fc354b
devin <deivnobowen@gmail.com>
2017-12-15 11:05:48 -0700

    1

--- sim/src/App.js
+++ sim/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import './reset.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      // sim 22c-2,4
+      <div className="home">
+        <div className="header"><img src={require('./assets/logo.png')} /> SHELFIE</div>
+        <div className="body">
+        <div className="shelf"><div className="sText">Shelf A</div></div>
+        <div className="shelf"><div className="sText">Shelf B</div></div>
+        <div className="shelf"><div className="sText">Shelf C</div></div>
+        <div className="shelf"><div className="sText">Shelf D</div></div>
+        </div>
       </div>
     );
   }
